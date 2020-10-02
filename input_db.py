aca23b1d1d826b129d8db7c222a46c6fd8af4fc8
Mano draugas <slkyper@gmail.com>
2020-10-02 20:50:29 +0000

    Update input_db.py

--- input_db.py
+++ input_db.py
@@ -1,6 +1,6 @@
-import getpass # tesiog nerodo passwordo ,kuri rasote terminale.
+import getpass # tesiog nerodo tekstine israiska passwordo ,kuri rasote terminale.
 
-host = input("Please enter your MySQL host : ")
-user = input("Please enter your MySQL user : ")
-password = getpass.getpass("Please enter your MySQL password: ")
-my_database_name = input("Please enter your MySQL database name : ")
+host = input("Please enter your MySQL host : ") # example host = localhost
+user = input("Please enter your MySQL user : ") # example user = root
+password = getpass.getpass("Please enter your MySQL password: ") # example password = 1234
+my_database_name = input("Please enter your MySQL database name : ") # example database_name = imdbData
